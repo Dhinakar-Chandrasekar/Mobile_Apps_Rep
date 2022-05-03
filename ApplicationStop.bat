92a798b42ed6dca46aec652eb0668e48ebc489d4
Dhinakar-Chandrasekar <99582854+Dhinakar-Chandrasekar@users.noreply.github.com>
2022-05-03 12:31:28 +0530

    Update ApplicationStop.bat

--- ApplicationStop.bat
+++ ApplicationStop.bat
@@ -1,12 +1,1 @@
-@echo off
-SET Appname1=WebStore
-SET Appname2=WebStoreAPI
-cd C:\WINDOWS\system32\inetsrv
-appcmd.exe stop apppool /apppool.name:"%Appname1%" || goto :error
-appcmd.exe stop site /site.name:"%Appname1%" || goto :error
-appcmd.exe stop apppool /apppool.name:"%Appname2%" || goto :error
-appcmd.exe stop site /site.name:"%Appname2%" || goto :error
-
-:error
-echo Failed with error #%errorlevel%.
 exit /b %errorlevel%
